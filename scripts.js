46bec7e8b33e005c0df64b3f7152d1fe39bf6985
uityh <matthew6600@gmail.com>
2023-04-10 15:32:13 -0400

    demo

--- scripts.js
+++ scripts.js
@@ -1,53 +1,53 @@
 $(document).ready(function () {
-    $(document).ready(function () {
-        $('[data-toggle="tooltip"]').tooltip();
-        var actions = $("table td:last-child").html();
+  $(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    var actions = $("table td:last-child").html();
 
-        $(document).on("click", ".add", function () {
-            var empty = false;
-            var input = $(this).parents("tr").find('input[type="text"]');
-            input.each(function () {
-                if (!$(this).val()) {
-                    $(this).addClass("error");
-                    empty = true;
-                } else {
-                    $(this).removeClass("error");
-                }
-            });
-            $(this).parents("tr").find(".error").first().focus();
-            if (!empty) {
-                input.each(function () {
-                    $(this).parent("td").html($(this).val());
-                });
-                $(this).parents("tr").find(".add, .edit").toggle();
-                $(".add-new").removeAttr("disabled");
-            }
+    $(document).on("click", ".add", function () {
+      var empty = false;
+      var input = $(this).parents("tr").find('input[type="text"]');
+      input.each(function () {
+        if (!$(this).val()) {
+          $(this).addClass("error");
+          empty = true;
+        } else {
+          $(this).removeClass("error");
+        }
+      });
+      $(this).parents("tr").find(".error").first().focus();
+      if (!empty) {
+        input.each(function () {
+          $(this).parent("td").html($(this).val());
         });
-        // Edit row on edit button click
-        // $(document).on("click", ".edit", function () {
-        //   $(this)
-        //     .parents("tr")
-        //     .find("td:not(:last-child)")
-        //     .each(function () {
-        //       $(this).html(
-        //         '<input type="text" class="form-control" value="' +
-        //           $(this).text() +
-        //           '">'
-        //       );
-        //     });
-        //   $(this).parents("tr").find(".add, .edit").toggle();
-        //   $(".add-new").attr("disabled", "disabled");
-        // });
-        // Delete row on delete button click
-        $(document).on("click", ".delete", function () {
-            $('[data-toggle="tooltip"]').tooltip("hide");
+        $(this).parents("tr").find(".add, .edit").toggle();
+        $(".add-new").removeAttr("disabled");
+      }
+    });
+    // Edit row on edit button click
+    // $(document).on("click", ".edit", function () {
+    //   $(this)
+    //     .parents("tr")
+    //     .find("td:not(:last-child)")
+    //     .each(function () {
+    //       $(this).html(
+    //         '<input type="text" class="form-control" value="' +
+    //           $(this).text() +
+    //           '">'
+    //       );
+    //     });
+    //   $(this).parents("tr").find(".add, .edit").toggle();
+    //   $(".add-new").attr("disabled", "disabled");
+    // });
+    // Delete row on delete button click
+    $(document).on("click", ".delete", function () {
+      $('[data-toggle="tooltip"]').tooltip("hide");
 
-            $(this).parents("tr").remove();
-            $(".add-new").removeAttr("disabled");
-        });
+      $(this).parents("tr").remove();
+      $(".add-new").removeAttr("disabled");
+    });
 
-        $("#new-table").click(function () {
-            var html = `<div class="table-wrapper">
+    $("#new-table").click(function () {
+      var html = `<div class="table-wrapper">
           <div class="table-title">
             <div class="row">
               <div class="col-sm-8">
@@ -72,56 +72,52 @@ $(document).ready(function () {
           </table>
         </div>`;
 
-            $(".table-responsive").append(html);
+      $(".table-responsive").append(html);
 
-            //get last add new button and add click event
-            var lastAddNew = $(".table-responsive").find(".add-new").last();
-            lastAddNew.click(function () {
-                //get parent and get table sibling
-                var table = $(this)
-                    .parent()
-                    .parent()
-                    .parent()
-                    .siblings("table");
-                var index = table.find("tbody tr:last-child").index();
-                var row =
-                    "<tr>" +
-                    '<td><input type="text" class="form-control" name="name" id="name"></td>' +
-                    '<td><input type="text" class="form-control" name="department" id="department"></td>' +
-                    '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
-                    "<td>" +
-                    actions +
-                    "</td>" +
-                    "</tr>";
-                table.append(row);
-                table
-                    .eq(index + 1)
-                    .find(".add, .edit")
-                    .toggle();
-                $('[data-toggle="tooltip"]').tooltip();
-            });
-        });
-
-        //add new row should add a new row to the table
-        $(".add-new").click(function () {
-            //get parent and get table sibling
-            var table = $(this).parent().parent().parent().siblings("table");
-            var index = table.find("tbody tr:last-child").index();
-            var row =
-                "<tr>" +
-                '<td><input type="text" class="form-control" name="name" id="name"></td>' +
-                '<td><input type="text" class="form-control" name="department" id="department"></td>' +
-                '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
-                "<td>" +
-                actions +
-                "</td>" +
-                "</tr>";
-            table.append(row);
-            table
-                .eq(index + 1)
-                .find(".add, .edit")
-                .toggle();
-            $('[data-toggle="tooltip"]').tooltip();
-        });
+      //get last add new button and add click event
+      var lastAddNew = $(".table-responsive").find(".add-new").last();
+      lastAddNew.click(function () {
+        //get parent and get table sibling
+        var table = $(this).parent().parent().parent().siblings("table");
+        var index = table.find("tbody tr:last-child").index();
+        var row =
+          "<tr>" +
+          '<td><input type="text" class="form-control" name="name" id="name"></td>' +
+          '<td><input type="text" class="form-control" name="department" id="department"></td>' +
+          '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
+          "<td>" +
+          actions +
+          "</td>" +
+          "</tr>";
+        table.append(row);
+        table
+          .eq(index + 1)
+          .find(".add, .edit")
+          .toggle();
+        $('[data-toggle="tooltip"]').tooltip();
+      });
     });
+
+    //add new row should add a new row to the table
+    $(".add-new").click(function () {
+      //get parent and get table sibling
+      var table = $(this).parent().parent().parent().siblings("table");
+      var index = table.find("tbody tr:last-child").index();
+      var row =
+        "<tr>" +
+        '<td><input type="text" class="form-control" name="name" id="name"></td>' +
+        '<td><input type="text" class="form-control" name="department" id="department"></td>' +
+        '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
+        "<td>" +
+        actions +
+        "</td>" +
+        "</tr>";
+      table.append(row);
+      table
+        .eq(index + 1)
+        .find(".add, .edit")
+        .toggle();
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+  });
 });
